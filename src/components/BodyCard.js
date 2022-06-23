c0a25e07f3d12e9fb7f6af58e0b7fdb2efb7dd5d
CarlAbel <caweb.cph@gmail.com>
2022-06-23 17:33:07 +0200

    Small tweaks on card lines

--- src/components/BodyCard.js
+++ src/components/BodyCard.js
@@ -1,16 +1,23 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react"
 
-const BodyCard = ({ type, colors, text }) => {
+import { useContext } from "react"
+import ThemeContext from "../context/ThemeContext"
+
+const BodyCard = ({ type, text }) => {
+  const colors = useContext(ThemeContext)
   const styles = {
     card: css`
+      color: ${colors.text_1};
       font-size: 11pt;
       font-weight: 300;
-      height: 38px;
-      opacity: 0.8;
+      height: 30px;
+      opacity: 0.7;
       width: 100%;
       overflow: hidden;
       text-overflow: ellipsis;
+      display: -webkit-box;
+      line-clamp: 2;
     `,
   }
   return <>{type === "textCard" && <p css={styles.card}>{text}</p>}</>
