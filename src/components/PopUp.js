02386c52de5b17bd9378f2a701b6ba8ffe8eb0fe
CarlAbel <caweb.cph@gmail.com>
2022-06-24 13:04:14 +0200

    small changes

--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -9,18 +9,18 @@ import { useContext } from "react"
 const PopUp = ({ popUp, popUpOpen }) => {
   const colors = useContext(ThemeContext)
   const styles = {
-    popup: css`
+    popUp: css`
       width: calc(100vw - 4rem);
-      height: 2rem;
+      height: 32px;
       position: fixed;
-      top: 2rem;
+      top: 64px;
       left: 50%;
       transform: translateX(-50%);
-      background: ${colors.primary_1};
-      z-index: 2000;
-      border-radius: 1.5rem;
+      background: ${colors.primaryColor_1};
+      z-index: 1000;
+      border-radius: 20px;
       opacity: 0;
-      padding: 0 2rem;
+      padding: 0 32px;
       transition: 0.5s;
       display: flex;
       justify-content: center;
@@ -36,21 +36,21 @@ const PopUp = ({ popUp, popUpOpen }) => {
         overflow-x: hidden;
       }
       & svg {
-        width: 2rem;
-        height: 2rem;
+        width: 30px;
+        height: 30px;
         transition: 0.7s;
         transform: scale(0.75) rotate(-90deg);
         opacity: 0;
       }
       ${popUpOpen &&
       `
-                    height: 10rem;
+                    height: 150px;
                     opacity: 1;
-                    padding: 2rem; 
+                    padding: 12px; 
                     pointer-events: all;
                     & svg {
                         transform: scale(1) rotate(0);
-                        opacity: 1
+                        opacity: 0.8
                     }
                     & p {
                         opacity: 1;
@@ -59,7 +59,7 @@ const PopUp = ({ popUp, popUpOpen }) => {
     `,
   }
   return (
-    <Link to="/archive" css={styles.popup}>
+    <Link to="/archive" css={styles.popUp}>
       <FeatherIcon icon="info" />
       <p>{popUpOpen && popUp}</p>
     </Link>
